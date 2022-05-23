a85374a05db209778f23f5e7ce631d11c321c144
JacobSinger42 <jesingermz@gmail.com>
2022-05-22 19:51:41 -0500

    Added Slope Field Class
    Added class for Slope Fields and framework for Conics class (0.1.2)

---
+++ thales/extras/conics.py
@@ -0,0 +1,28 @@
+import math 
+import thales as th 
+import pygame 
+from pygame.locals import * 
+
+class Circle: 
+    ... 
+
+class ICircle (Circle): 
+    ...
+
+class Ellipse: 
+    ...
+
+class IEllipse (Ellipse): 
+    ...
+
+class Parabola: 
+    ...
+
+class IParabola (Parabola): 
+    ...
+
+class Hyperbola: 
+    ...
+     
+class IHyperbola (Hyperbola): 
+    ...
